331f869478169d6a6bc594dfdcefa6a44f778f4c
Jasmine Carter <heyjasminec@gmail.com>
2021-01-19 18:25:23 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+Material.destroy_all
+
+project = Project.create(name: "Macrame Table Runner", description: "Table runner for all seasons", completed: false)
+
+material = Material.create(name: "White Cording", project_id: 1, cost: 15.30, purchased: false, quantity: 2)
+material2 = Material.create(name: "Dowel", project_id: 1, cost: 0.50, purchased: false, quantity: 1)
