e94bde689012929ebe1f90679272d44e901e3f63
Jasmine Carter <heyjasminec@gmail.com>
2021-01-19 18:56:53 -0500

    Build out basic controller actiosn

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,12 +2,23 @@ class Api::V1::ProjectsController < ApplicationController
 
 
   def index
-
+    @projects = Project.all
+    render json: @projects
   end
 
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      render json: @project
+    else
+      render json: {error: 'Error creating your project'} #generate maybe more unique/specific error handling
+    end
+  end
 
+  def show
+    @project = Project.find(params[:id])
+    render json: @project
   end
 
 
@@ -15,12 +26,17 @@ class Api::V1::ProjectsController < ApplicationController
 
   end
 
-  def delete
-
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
   end
 
 
+  private
 
+  def project_params
+    params.require(:project).permit(:name, :description, :completed, :image)
+  end
 
 
 end
