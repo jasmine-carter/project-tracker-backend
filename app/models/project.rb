f8cac5e8720c668376ace2021c17dd61ec831771
Jasmine Carter <heyjasminec@gmail.com>
2021-01-19 18:40:46 -0500

    Create instance method in Project model to update project instance total cost

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ApplicationRecord
   has_many :materials
   validates :name, presence: true
-  
+
+
+  def update_total_cost
+      self.materials.each do |material|
+      self.cost += material.cost
+    end
+  end
+
+
 end
