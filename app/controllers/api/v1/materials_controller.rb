e7365889e2195e30cdb3eb73350c4b25e11b6743
Jasmine Carter <heyjasminec@gmail.com>
2021-01-19 19:04:01 -0500

    Add index and create action logic to Material controller

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -1,13 +1,22 @@
-class MaterialsController < ApplicationController
+class Api::V1::MaterialsController < ApplicationController
 
+  before_action :set_project
 
   def index
-
+    @materials = @project.materials
+      if @materials.count >= 1
+        render json: @materials
+      else
+        render json: {error: "This project has no materials yet, add some?"}
   end
 
 
   def create
-
+    @material = Material.new(material_params)
+    if @material.save
+      render json: @material
+    else
+      render json: {error: 'Error creating your material'}#create more specific error handling
   end
 
 
@@ -15,6 +24,15 @@ class MaterialsController < ApplicationController
 
   end
 
+  private
 
+  def set_project
+    @project = Project.find(params[:project_id])
+
+  end
+
+  def material_params
+      params.require(:material).permit(:name, :cost, :purchased, :quantity, :project_id)
+  end
 
 end
