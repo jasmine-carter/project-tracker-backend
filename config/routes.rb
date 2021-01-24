ac2f74835072cfa7bf21336269c559edc00ae0d0
Jasmine Carter <heyjasminec@gmail.com>
2021-01-24 13:22:43 -0500

    Move comments routes nested under project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :comments
   namespace :api do
     namespace :v1 do
       resources :projects do
           resources :materials
+          resources :comments
       end
     end
   end
