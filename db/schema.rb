9659a107b27d8827bfa06ddbe35569418f7f6fb3
Jasmine Carter <heyjasminec@gmail.com>
2021-01-24 13:19:56 -0500

    Run comment creation migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_234833) do
+ActiveRecord::Schema.define(version: 2021_01_24_181637) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "materials", force: :cascade do |t|
     t.string "name"
