11ebc5140fbec2a3ecfbb408d4af0ed37945c6a3
Jasmine Carter <heyjasminec@gmail.com>
2021-01-24 13:26:32 -0500

    Move comment controller to api/v1 namespace and build out crud actions

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+
+  before_action :set_project
+
+  def index
+    @comments = @projects.comments
+      if @comments.count >= 1
+        render json: @comments
+      else
+        render json: {error: "There are no comments here yet"}
+      end
+  end
+
+  def create
+    @comment = @project.comments.find_by(id: params[:id])
+    @comment.save
+    @project.save
+    render json: @project
+  end
+
+  def destroy
+    @comment = @project.comments.find_by(id: params[:id])
+    @comment.destroy
+    @project.save
+    render json: @project
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :project_id)
+  end
+
+end
